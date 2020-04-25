3420091c7479d3222336611e993a20538a8aa04c
ta1H3n <virma21@gmail.com>
2020-04-25 18:23:27 +0200

    Capture inner command exception to sentry if available instead

--- Namiko/Program.cs
+++ Namiko/Program.cs
@@ -249,7 +249,10 @@ namespace Namiko
         {
             if (logMessage.Exception is CommandException cmdException)
             {
-                SentrySdk.CaptureException(cmdException);
+                if (cmdException.InnerException != null)
+                    SentrySdk.CaptureException(cmdException.InnerException);
+                else
+                    SentrySdk.CaptureException(cmdException);
             }
         }
 
