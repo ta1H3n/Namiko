15b44a98e9439e22e4b5989fa0222f248f6b3d8d
ta1H3n <virma21@gmail.com>
2020-06-26 18:54:12 +0200

    Front page RC2

--- Website/ClientApp/src/components/Home.js
+++ Website/ClientApp/src/components/Home.js
@@ -8,8 +8,8 @@ export class Home extends Component {
         return (
             <>
                 <div class="container-image front-page-cover">
-                    <img src="/images/banner.jpg" className="img-banner" alt="banner"/>
-                    <div class="centered">
+                    <img src="/images/banner.jpg" className="img-banner" alt="banner" />
+                    <div className="centered-left">
                         <h1 className="text-center title-text"><strong>Namiko</strong></h1>
                         <h3 className="text-center title-text">Anime Powered Discord Bot</h3>
                         <div className="btn-group color-discord round-sm mt-2" role="group">
@@ -21,6 +21,7 @@ export class Home extends Component {
                         </div>
                     </div>
                 </div>
+
                 <Container>
                     <div className="full-size">
                         <div className="container mt-2">
@@ -39,7 +40,7 @@ export class Home extends Component {
                         <div className="container mt-5">
                             <div className="row module-row">
                                 <div className="col-md-5 col-sm-12">
-                                    <div className="color1 round module">
+                                    <div className="color1 round module-left">
                                         <h2 id="waifus">Waifus</h2>
                                         <p>Namiko has high quality tiered waifus from anime, games and more. They were all carefully made by our insiders, per request and per popularity.</p>
                                     </div>
@@ -50,13 +51,13 @@ export class Home extends Component {
                             </div>
                         </div>
 
-                        <div className="container mt-3">
+                        <div className="container mt-4">
                             <div className="row module-row">
                                 <div className="col-md-7 col-sm-12">
                                     <img src="/images/banroulette.png" className="img-module horizontal-center" alt="" />
                                 </div>
                                 <div className="col-md-5 col-sm-12">
-                                    <div className="color3 round module align-middle">
+                                    <div className="color3 round module-right">
                                         <h2 id="banroulette">Banroulette</h2>
                                         <p>Start a banroulette where users can join for a chance to be banned, while others share a pool of rewards!</p>
                                     </div>
@@ -64,10 +65,10 @@ export class Home extends Component {
                             </div>
                         </div>
 
-                        <div className="container mt-3">
+                        <div className="container mt-4">
                             <div className="row module-row">
                                 <div className="col-md-5 col-sm-12">
-                                    <div className="color1 round module">
+                                    <div className="color1 round module-left">
                                         <h2 id="user-profile">User Profile</h2>
                                         <p>Full profile customization. Set a custom image, quote and a color for yourself. Show off your waifu on your profile!</p>
                                     </div>
@@ -78,13 +79,13 @@ export class Home extends Component {
                             </div>
                         </div>
 
-                        <div className="container mt-3">
+                        <div className="container mt-4">
                             <div className="row module-row">
                                 <div className="col-md-7 col-sm-12">
                                     <img src="/images/music.png" className="img-module horizontal-center" alt="" />
                                 </div>
                                 <div className="col-md-5 col-sm-12">
-                                    <div className="color3 round module">
+                                    <div className="color3 round module-right">
                                         <h2 id="music">Music</h2>
                                         <p>High quality music from many sources including Youtube, Soundcloud and Mixer. Create and save your own playlists! Namiko will talk to you in voice chat ^^</p>
                                     </div>
@@ -92,10 +93,10 @@ export class Home extends Component {
                             </div>
                         </div>
 
-                        <div className="container mt-3">
+                        <div className="container mt-4">
                             <div className="row module-row">
                                 <div className="col-md-5 col-sm-12">
-                                    <div className="color1 round module">
+                                    <div className="color1 round module-left">
                                         <h2 id="currency-and-gambling">Currency & Gambling</h2>
                                         <p>Server exclusive currency for buying waifus, gambling and trading. Complex scaling algorithms are used to provide both users and Namiko with a sufficient balance without bloating the economy!</p>
                                     </div>
@@ -106,13 +107,13 @@ export class Home extends Component {
                             </div>
                         </div>
 
-                        <div className="container mt-3">
+                        <div className="container mt-4">
                             <div className="row module-row">
                                 <div className="col-md-7 col-sm-12">
                                     <img src="/images/reaction-images.png" className="img-module horizontal-center" alt="" />
                                 </div>
                                 <div className="col-md-5 col-sm-12">
-                                    <div className="color3 round module">
+                                    <div className="color3 round module-right">
                                         <h2 id="reaction-images">Reaction Images</h2>
                                         <p>Over 200 reaction image commands, and over 2000 images. Such as pat, hug, kick, hydrate and much more!</p>
                                     </div>
@@ -120,10 +121,10 @@ export class Home extends Component {
                             </div>
                         </div>
 
-                        <div className="container mt-3">
+                        <div className="container mt-4">
                             <div className="row module-row">
                                 <div className="col-md-5 col-sm-12">
-                                    <div className="color1 round module">
+                                    <div className="color1 round module-left">
                                         <h2 id="web-commands">Web Commands</h2>
                                         <p>Commands using various internet services. Subscribe to a subreddit, search for anime, get image sources and more!</p>
                                     </div>
@@ -135,6 +136,39 @@ export class Home extends Component {
                         </div>
                     </div>
                 </Container>
+                <div className="mt-5 home-footer">
+                    <Container>
+                        <div className="container row mt-4 mr-0 ml-0">
+                            <div className="horizontal-center text-center">
+                                <h1 className="title-text"><strong>Try Namiko Now!</strong></h1>
+                            </div>
+                        </div>
+                        <div className="container row mt-2 mr-0 ml-0">
+                            <div className="horizontal-center">
+                                <div className="btn-group color6 round-sm mt-2 mr-2" role="group">
+                                    <div className="btn btn-static color6 small-padding">
+                                        <img src="/images/Discord-Logo-White.png" className="img" />
+                                    </div>
+                                    <a href="https://discordapp.com/oauth2/authorize?client_id=418823684459855882&scope=bot&permissions=268707844" type="button" className="btn color6 color6-hover">Invite&nbsp;Namiko</a>
+                                    <a href="https://discord.gg/W6Ru5sM" type="button" className="btn color6 color6-hover">Join&nbsp;Server</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container row mt-2 mr-0 ml-0">
+                            <div className="horizontal-center">
+                                <div className="btn-group color6 round-sm mt-2 mr-2" role="group">
+                                    <a href="/Commands" type="button" className="btn color6 color6-hover">Commands</a>
+                                </div>
+                                <div className="btn-group color6 round-sm mt-2 mr-2" role="group">
+                                    <a href="/Pro" type="button" className="btn color6 color6-hover">Get Pro</a>
+                                </div>
+                                <div className="btn-group color6 round-sm mt-2 mr-2" role="group">
+                                    <a href="/WaifuShop/418900885079588884" type="button" className="btn color6 color6-hover">Web dashboard</a>
+                                </div>
+                            </div>
+                        </div>
+                    </Container>
+                </div>
             </>
         );
     }
