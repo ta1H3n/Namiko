d092899422ba83ecbb1e16364b56f5aab76bf378
ta1H3n <virma21@gmail.com>
2020-09-12 18:53:33 +0200

    Navbar collapse

--- Website/ClientApp/src/components/NavMenu.js
+++ Website/ClientApp/src/components/NavMenu.js
@@ -26,24 +26,25 @@ export class NavMenu extends Component {
     render() {
         return (
             <header>
-                <Navbar className="mb-3 namiko-navbar" variant="dark">
+                <Navbar collapseOnSelect expand="md" className="mb-3 namiko-navbar" variant="dark">
                     <Container>
                         <Navbar.Brand as={Link} to="/">
                             <img
-                            src="/images/NamikoMoe.png"
-                            width="auto"
-                            height="30"
-                            className="d-inline-block align-top"
-                            alt="Namiko Moe logo"
+                                src="/images/NamikoMoe.png"
+                                width="auto"
+                                height="30"
+                                className="d-inline-block align-top"
+                                alt="Namiko Moe logo"
                             />
                         </Navbar.Brand>
-                        <Navbar.Collapse isOpen={!this.state.collapsed}>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="mr-auto">
                                 <Nav.Link as={Link} to="/Guide">Guide</Nav.Link>
                                 <Nav.Link as={Link} to="/Commands">Commands</Nav.Link>
                                 <Nav.Link as={Link} to="/Waifus">Waifus</Nav.Link>
                             </Nav>
-                            <LoginMenu/>
+                            <LoginMenu />
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
