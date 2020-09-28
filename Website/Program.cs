99ac3e9322e8313a1f971240e6c188c05461524c
ta1H3n <virma21@gmail.com>
2020-09-28 20:56:57 +0200

    Add sentry

--- Website/Program.cs
+++ Website/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Website
 {
@@ -15,6 +16,7 @@ namespace Website
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSentry(Environment.GetEnvironmentVariable("SENTRY"));
                 });
     }
 }
