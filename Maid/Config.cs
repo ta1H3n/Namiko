45df11b3f991f7df1a7c1a8a3620bcf9f6a4314e
taiHen <virma21@gmail.com>
2021-04-27 13:52:38 +0200

    Add save capability to config

--- Maid/Config.cs
+++ Maid/Config.cs
@@ -19,19 +19,37 @@ namespace Maid
 
         static Config()
         {
-            string JSON = "";
-            var JSONLocation = (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")) switch
+            string json = "";
+            using (var stream = new FileStream(GetPath(), FileMode.Open, FileAccess.Read))
+            using (var reader = new StreamReader(stream))
+            {
+                json = reader.ReadToEnd();
+                reader.Close();
+            }
+
+            JsonConvert.DeserializeObject<Config>(json);
+        }
+
+        public static void Save()
+        {
+            var config = new Config();
+            var json = JsonConvert.SerializeObject(config, Formatting.Indented);
+
+            using (var stream = new FileStream(GetPath(), FileMode.Open, FileAccess.Write))
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(json);
+                writer.Close();
+            }
+        }
+
+        private static string GetPath()
+        {
+            return (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")) switch
             {
                 "Development" => Assembly.GetEntryAssembly().Location.Replace(@"bin\Debug\netcoreapp3.1\Maid.dll", @"appsettings.json"),
                 _ => Assembly.GetEntryAssembly().Location.Replace(@"Maid.dll", @"appsettings.json"),
             };
-            using (var Stream = new FileStream(JSONLocation, FileMode.Open, FileAccess.Read))
-            using (var ReadSettings = new StreamReader(Stream))
-            {
-                JSON = ReadSettings.ReadToEnd();
-            }
-
-            JsonConvert.DeserializeObject<Config>(JSON);
         }
     }
 }
